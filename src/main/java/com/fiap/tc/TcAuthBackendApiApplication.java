720642d221b1316c7d08d2fae9733709fc7dc3f7
Myller Sakaguchi Lobo <myller.lobo@omni.com.br>
2025-01-22 15:36:35 -0300

    add level cache get customer

--- src/main/java/com/fiap/tc/TcAuthBackendApiApplication.java
+++ src/main/java/com/fiap/tc/TcAuthBackendApiApplication.java
@@ -4,10 +4,12 @@ import com.fiap.tc.infrastructure.core.security.property.OriginApiProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableConfigurationProperties(OriginApiProperty.class)
+@EnableScheduling
 public class TcAuthBackendApiApplication {
 
     public static void main(String[] args) {
